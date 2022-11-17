ea1659ebd026d71040758e41929a5e0b4ae54de1
joao fernandes <joalo.fernandes@cognizant.com>
2022-11-17 17:03:16 +0000

    added the message controller

---
+++ src/main/java/com/example/springbootazure/controller/MessageController.java
@@ -0,0 +1,13 @@
+package com.example.springbootazure.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @GetMapping("/message")
+    public String message(){
+        return "My app is deployed in Azure!";
+    }
+}
